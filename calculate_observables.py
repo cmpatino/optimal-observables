1e9ff0c76f32d8ec959e60341b87997c2631999c
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2020-12-10 23:27:40 -0500

    Create orthonormal basis for observables
    Develop function to create the basis that is going to be used for
    calculating the observables.

--- calculate_observables.py
+++ calculate_observables.py
@@ -17,3 +17,16 @@ def boost_to_frame(p_particle, p_frame):
 def boost_to_com(p1, p2):
     p_com = p1 + p2
     return boost_to_frame(p1, p_com), boost_to_frame(p2, p_com), p_com
+
+
+def create_basis(top_p_com):
+    k_hat = top_p_com[:3] / np.linalg.norm(top_p_com[:3])
+    p_hat = np.array([0, 0, 1])
+    theta = np.arccos(np.sum(k_hat * p_hat))
+
+    n_hat = np.cross(p_hat, k_hat) / np.sin(theta)
+    r_hat = (p_hat - (k_hat * np.cos(theta))) / np.sin(theta)
+    if theta > (np.pi / 2):
+        n_hat *= -1
+        r_hat *= -1
+    return k_hat, r_hat, n_hat
