9626417d67a29d4c22c585a32b9ef3ea98d794ef
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2020-12-12 00:54:52 -0500

    Vectorize boosts and basis creation

--- calculate_observables.py
+++ calculate_observables.py
@@ -2,15 +2,15 @@ import numpy as np
 
 
 def boost_to_frame(p_particle, p_frame):
-    b = -p_frame[:3] / p_frame[3]
-    b2 = np.sum(b**2)
+    b = - p_frame[:, :3] / p_frame[:, 3:]
+    b2 = np.sum(b**2, axis=1, keepdims=True)
     gamma = 1 / np.sqrt(1 - b2)
-    bp = np.sum(p_particle[:3] * b)
-    gamma2 = max((gamma - 1) / b2, 0)
+    bp = np.sum(p_particle[:, :3] * b, axis=1, keepdims=True)
+    gamma2 = (gamma - 1) / b2
 
-    p_particle_boosted = np.zeros_like(p_particle)
-    p_particle_boosted[:3] = p_particle[:3] + (gamma2 * bp * b) + (gamma * b * p_particle[3])
-    p_particle_boosted[3] = gamma * (p_particle[3] + bp)
+    space_coords = p_particle[:, :3] + (gamma2 * bp * b) + (gamma * b * p_particle[:, 3:])
+    time_coord = gamma * (p_particle[:, 3:] + bp)
+    p_particle_boosted = np.concatenate([space_coords, time_coord], axis=1)
     return p_particle_boosted
 
 
@@ -20,8 +20,8 @@ def boost_to_com(p1, p2):
 
 
 def create_basis(top_p_com):
-    k_hat = top_p_com[:3] / np.linalg.norm(top_p_com[:3])
-    p_hat = np.array([0, 0, 1])
+    k_hat = top_p_com[:, :3] / np.linalg.norm(top_p_com[:, :3])
+    p_hat = np.array([0, 0, 1]).reshape(1, -1)
     theta = np.arccos(np.sum(k_hat * p_hat))
 
     n_hat = np.cross(p_hat, k_hat) / np.sin(theta)
