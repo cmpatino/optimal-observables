efc2df3bc1d68f768999a2ac287d037caefd3beb
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2021-04-13 23:19:33 -0500

    Adjustments to calculate entire observable matrix

--- optimal_observables/observables.py
+++ optimal_observables/observables.py
@@ -2,13 +2,15 @@ import numpy as np
 
 
 def boost_to_frame(p_particle, p_frame):
-    b = - p_frame[:, :3] / p_frame[:, 3:]
-    b2 = np.sum(b**2, axis=1, keepdims=True)
+    b = -p_frame[:, :3] / p_frame[:, 3:]
+    b2 = np.sum(b ** 2, axis=1, keepdims=True)
     gamma = 1 / np.sqrt(1 - b2)
     bp = np.sum(p_particle[:, :3] * b, axis=1, keepdims=True)
     gamma2 = np.clip((gamma - 1) / b2, a_min=0, a_max=None)
 
-    space_coords = p_particle[:, :3] + (gamma2 * bp * b) + (gamma * b * p_particle[:, 3:])
+    space_coords = (
+        p_particle[:, :3] + (gamma2 * bp * b) + (gamma * b * p_particle[:, 3:])
+    )
     time_coord = gamma * (p_particle[:, 3:] + bp)
     p_particle_boosted = np.concatenate([space_coords, time_coord], axis=1)
     return p_particle_boosted
@@ -35,15 +37,16 @@ def create_basis(top_p_com):
 def calculate_cosine_obs(p_particle, k_hat, r_hat, n_hat):
     basis_change = np.linalg.inv(np.stack([k_hat, r_hat, n_hat], axis=-1))
     p_new_basis = np.matmul(basis_change, np.expand_dims(p_particle[:, :3], axis=-1))
-    obs = (p_new_basis[:, :3] / np.linalg.norm(p_new_basis, axis=1, keepdims=True)).squeeze(-1)
-    cos_k = obs[:, 0]
-    cos_r = obs[:, 1]
-    cos_n = obs[:, 2]
+    obs = (
+        p_new_basis[:, :3] / np.linalg.norm(p_new_basis, axis=1, keepdims=True)
+    ).squeeze(-1)
+    cos_k = obs[:, 0:1]
+    cos_r = obs[:, 1:2]
+    cos_n = obs[:, 2:3]
     return cos_k, cos_r, cos_n
 
 
-def obs_matrix(p_l_t, p_l_tbar, p_top, p_tbar):
-    obs_dict = dict()
+def get_matrix(p_l_t, p_l_tbar, p_top, p_tbar):
     p_top_com, p_tbar_com, p_com = boost_to_com(p_top, p_tbar)
     k_hat, r_hat, n_hat = create_basis(p_top_com)
 
@@ -52,10 +55,25 @@ def obs_matrix(p_l_t, p_l_tbar, p_top, p_tbar):
 
     cos_k1, cos_r1, cos_n1 = calculate_cosine_obs(p_l_t_frame, k_hat, r_hat, n_hat)
     cos_k2, cos_r2, cos_n2 = calculate_cosine_obs(p_l_tbar_frame, k_hat, r_hat, n_hat)
-    obs_dict["cos_k1"] = cos_k1
-    obs_dict["cos_r1"] = cos_r1
-    obs_dict["cos_n1"] = cos_n1
-    obs_dict["cos_k2"] = cos_k2
-    obs_dict["cos_r2"] = cos_r2
-    obs_dict["cos_n2"] = cos_n2
-    return obs_dict
+
+    obs_matrix = np.concatenate(
+        [
+            cos_k1,
+            cos_k2,
+            cos_r1,
+            cos_r2,
+            cos_n1,
+            cos_n2,
+            cos_k1 * cos_k2,
+            cos_r1 * cos_r2,
+            cos_n1 * cos_n2,
+            (cos_r1 * cos_k2) + (cos_k1 * cos_r2),
+            (cos_r1 * cos_k2) - (cos_k1 * cos_r2),
+            (cos_n1 * cos_r2) + (cos_r1 * cos_n2),
+            (cos_n1 * cos_r2) - (cos_r1 * cos_n2),
+            (cos_n1 * cos_k2) + (cos_k1 * cos_n2),
+            (cos_n1 * cos_k2) - (cos_k1 * cos_n2),
+        ],
+        axis=1,
+    )
+    return obs_matrix
