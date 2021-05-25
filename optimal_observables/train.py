d42c099878b93bc87784e0feecbdc9e84a9c5342
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2021-05-24 23:50:26 -0500

    Add MLflow logger

--- optimal_observables/train.py
+++ optimal_observables/train.py
@@ -2,13 +2,14 @@ import pytorch_lightning as pl
 
 from argparse import ArgumentParser
 from torch.utils.data import DataLoader, random_split
+from pytorch_lightning.loggers import MLFlowLogger
 
 from data import ConditionedObservablesFC
 from models import FullyConnected
 
 reco_paths = [
     "../reconstructed_events/SM_spin-ON_100k_0",
-    "../reconstructed_events/SM_spin-ON_100k_20210425"
+    "../reconstructed_events/SM_spin-ON_100k_20210425",
 ]
 n_out_samples = 10000
 
@@ -22,8 +23,7 @@ full_dataset = ConditionedObservablesFC(
 n_train = int(len(full_dataset) * 0.7)
 n_val = len(full_dataset) - n_train
 train_dataset, val_dataset = random_split(
-    dataset=full_dataset,
-    lengths=[n_train, n_val]
+    dataset=full_dataset, lengths=[n_train, n_val]
 )
 
 train_dataloader = DataLoader(train_dataset, batch_size=1, num_workers=4)
@@ -37,20 +37,22 @@ hparams.output_size = n_out_samples
 biases = full_dataset.biases
 model = FullyConnected(hparams=hparams, biases=biases)
 
+mlf_logger = MLFlowLogger(experiment_name="SM_spin-ON", tracking_uri="file:./mlruns/")
+
 checkpoint_callback = pl.callbacks.ModelCheckpoint(
-    monitor='val_loss',
-    dirpath='model_ckpts/',
-    filename='model-{epoch:02d}-{val_loss:.2f}',
+    monitor="val_loss",
+    dirpath="model_ckpts/models_SM_spin-ON",
+    filename="model-{epoch:02d}-{val_loss:.2f}",
     save_top_k=1,
-    mode='min',
+    mode="min",
 )
 early_stop_callback = pl.callbacks.early_stopping.EarlyStopping(
-   monitor='val_loss',
-   min_delta=0.00,
-   patience=3,
-   verbose=False,
-   mode='min'
+    monitor="val_loss", min_delta=0.00, patience=3, verbose=False, mode="min"
 )
-trainer = pl.Trainer(max_epochs=10000, callbacks=[checkpoint_callback, early_stop_callback])
+trainer = pl.Trainer(
+    logger=mlf_logger,
+    max_epochs=10000,
+    callbacks=[checkpoint_callback, early_stop_callback],
+)
 
 trainer.fit(model, train_dataloader, val_dataloader)
