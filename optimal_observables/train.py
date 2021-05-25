7a22b91c8ecef0760e4d266ff5c781c26923a40a
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2021-05-24 23:20:13 -0500

    Define callbacks for training and val
    + Include callbacks for model checkpoints and early stopping.
    + Split dataset into training and validation sets.

--- optimal_observables/train.py
+++ optimal_observables/train.py
@@ -1,7 +1,7 @@
 import pytorch_lightning as pl
 
 from argparse import ArgumentParser
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 from data import ConditionedObservablesFC
 from models import FullyConnected
@@ -10,21 +10,47 @@ reco_paths = [
     "../reconstructed_events/SM_spin-ON_100k_0",
     "../reconstructed_events/SM_spin-ON_100k_20210425"
 ]
-train_dataset = ConditionedObservablesFC(
+n_out_samples = 10000
+
+full_dataset = ConditionedObservablesFC(
     reconstructions_paths=reco_paths,
+    n_out_samples=n_out_samples,
     low_exp=-5,
     high_exp=5,
     n_exp=1000,
 )
-train_dataloader = DataLoader(train_dataset, batch_size=1, num_workers=0)
+n_train = int(len(full_dataset) * 0.7)
+n_val = len(full_dataset) - n_train
+train_dataset, val_dataset = random_split(
+    dataset=full_dataset,
+    lengths=[n_train, n_val]
+)
+
+train_dataloader = DataLoader(train_dataset, batch_size=1, num_workers=4)
+val_dataloader = DataLoader(val_dataset, batch_size=1, num_workers=4)
 
 parser = ArgumentParser()
 parser = FullyConnected.add_model_specific_args(parser)
 hparams = parser.parse_args()
-hparams.input_size = train_dataset.n_observables
-hparams.output_size = train_dataset.n_samples
-biases = train_dataset.biases
+hparams.input_size = full_dataset.n_observables
+hparams.output_size = n_out_samples
+biases = full_dataset.biases
 model = FullyConnected(hparams=hparams, biases=biases)
-trainer = pl.Trainer(max_epochs=10000)
 
-trainer.fit(model, train_dataloader)
+checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    monitor='val_loss',
+    dirpath='model_ckpts/',
+    filename='model-{epoch:02d}-{val_loss:.2f}',
+    save_top_k=1,
+    mode='min',
+)
+early_stop_callback = pl.callbacks.early_stopping.EarlyStopping(
+   monitor='val_loss',
+   min_delta=0.00,
+   patience=3,
+   verbose=False,
+   mode='min'
+)
+trainer = pl.Trainer(max_epochs=10000, callbacks=[checkpoint_callback, early_stop_callback])
+
+trainer.fit(model, train_dataloader, val_dataloader)
