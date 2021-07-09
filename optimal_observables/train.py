e15beadea472bf62a3dc77b32c24f264fc4856ed
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2021-07-08 23:45:37 -0500

    Setup full MLflow logging
    + Save model as artifact so each model is associated with a run.
    + Decouple params from train.py for easier setup and version tracking.

--- optimal_observables/train.py
+++ optimal_observables/train.py
@@ -1,26 +1,16 @@
+import mlflow
 import pytorch_lightning as pl
 
 from argparse import ArgumentParser
 from torch.utils.data import DataLoader, random_split
 from pytorch_lightning.loggers import MLFlowLogger
 
+import config
 from data import ConditionedObservablesFC
 from models import FullyConnected
 
-reco_paths = [
-    "../reconstructed_events/SM_spin-OFF_100k_0",
-    "../reconstructed_events/SM_spin-OFF_100k_20210429",
-]
-n_out_samples = 10000
 
-full_dataset = ConditionedObservablesFC(
-    reconstructions_paths=reco_paths,
-    n_out_samples=n_out_samples,
-    low_exp=-5,
-    high_exp=5,
-    n_exp=2000,
-    only_cosine_terms=True
-)
+full_dataset = ConditionedObservablesFC(**config.dataset_config)
 n_train = int(len(full_dataset) * 0.7)
 n_val = len(full_dataset) - n_train
 train_dataset, val_dataset = random_split(
@@ -34,15 +24,18 @@ parser = ArgumentParser()
 parser = FullyConnected.add_model_specific_args(parser)
 hparams = parser.parse_args()
 hparams.input_size = full_dataset.n_observables
-hparams.output_size = n_out_samples
+hparams.output_size = config.dataset_config["n_out_samples"]
 biases = full_dataset.biases
 model = FullyConnected(hparams=hparams, biases=biases)
 
-mlf_logger = MLFlowLogger(experiment_name="SM_spin-OFF", tracking_uri="file:./mlruns/")
+mlf_logger = MLFlowLogger(
+    experiment_name=f"{config.process}-pl",
+    tracking_uri="file:./mlruns/",
+)
 
 checkpoint_callback = pl.callbacks.ModelCheckpoint(
     monitor="val_loss",
-    dirpath="model_ckpts/models_SM_spin-OFF",
+    dirpath=f"model_ckpts/{config.process}",
     filename="model",
     save_top_k=1,
     mode="min",
@@ -56,4 +49,9 @@ trainer = pl.Trainer(
     callbacks=[checkpoint_callback, early_stop_callback],
 )
 
-trainer.fit(model, train_dataloader, val_dataloader)
+mlflow.set_experiment(config.process)
+mlflow.pytorch.autolog()
+with mlflow.start_run(tags={"pl-source": mlf_logger.run_id}) as run:
+    mlflow.log_params(config.dataset_config)
+    mlflow.log_params(vars(hparams))
+    trainer.fit(model, train_dataloader, val_dataloader)
