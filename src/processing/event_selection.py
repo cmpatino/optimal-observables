1359d5c35f2e6c464d6cc496eaac110baacad7d5
cmpatino <carlosmiguelpatino@gmail.com>
2022-02-15 23:35:47 -0500

    Decouple selection steps for each particle type

--- src/processing/event_selection.py
+++ src/processing/event_selection.py
@@ -1,119 +1,173 @@
+from typing import Literal
+
+import awkward as ak
 import numpy as np
-from awkward.array.jagged import JaggedArray
+from tqdm import tqdm
 
 import processing.kinematics as kinematics
 
 
-def select_jet(events) -> JaggedArray:
+def select_jet(events) -> ak.Array:
     """Create boolean mask to apply jet selection criteria from ATLAS
 
     :param events: Delphes event TTree containing
     :type events: TTree
     :return: boolean mask to select jets in events
-    :rtype: JaggedArray
+    :rtype: Array
     """
-    jet_pt = events["Jet.PT"].array()
-    jet_phi = events["Jet.Phi"].array()
-    jet_eta = events["Jet.Eta"].array()
-    electron_phi = events["Electron.Phi"].array()
-    electron_eta = events["Electron.Eta"].array()
-    muon_phi = events["Muon.Phi"].array()
-    muon_eta = events["Muon.Eta"].array()
 
-    pt_mask = jet_pt > 25
-    eta_mask = np.abs(jet_eta) < 2.5
-    electron_dR_mask = []
-    muon_dR_mask = []
-    for event_idx in range(len(events)):
-        jet_phi_idx = jet_phi[event_idx]
-        jet_eta_idx = jet_eta[event_idx]
-        electron_dR_event_mask = np.ones_like(jet_phi_idx, dtype=int)
-        muon_dR_event_mask = np.ones_like(jet_phi_idx, dtype=int)
-        for elec_idx in range(len(electron_phi[event_idx])):
-            dPhi = kinematics.normalize_dPhi(jet_phi_idx - electron_phi[event_idx][elec_idx])
-            dEta = jet_eta_idx - electron_eta[event_idx][elec_idx]
-            dR = np.sqrt(dPhi**2 + dEta**2)
-            electron_dR_event_mask *= (dR > 0.2)
-        for muon_idx in range(len(muon_phi[event_idx])):
-            dPhi = kinematics.normalize_dPhi(jet_phi_idx - muon_phi[event_idx][muon_idx])
-            dEta = jet_eta_idx - muon_eta[event_idx][muon_idx]
-            dR = np.sqrt(dPhi**2 + dEta**2)
-            muon_dR_event_mask *= (dR > 0.4)
-        electron_dR_mask.append(electron_dR_event_mask.astype(bool))
-        muon_dR_mask.append(muon_dR_event_mask.astype(bool))
-    electron_dR_mask = JaggedArray.fromiter(electron_dR_mask)
-    muon_dR_mask = JaggedArray.fromiter(muon_dR_mask)
+    pt_mask = get_jets_pt_mask(events=events)
+    eta_mask = get_jets_eta_mask(events=events)
+    electron_dR_mask = get_jet_to_lepton_mask(
+        events=events, lepton="Electron", min_dR=0.2
+    )
+    muon_dR_mask = get_jet_to_lepton_mask(events=events, lepton="Muon", min_dR=0.4)
     mask = pt_mask * eta_mask * electron_dR_mask * muon_dR_mask
     return mask
 
 
-def select_electron(events) -> JaggedArray:
+def get_jets_pt_mask(events) -> ak.Array:
+    jet_pt = events["Jet.PT"].array()
+    pt_mask = jet_pt > 25
+    return pt_mask
+
+
+def get_jets_eta_mask(events) -> ak.Array:
+    jet_eta = events["Jet.Eta"].array()
+    eta_mask = np.abs(jet_eta) < 2.5
+    return eta_mask
+
+
+def get_jet_to_lepton_mask(
+    events, lepton: Literal["Electron", "Muon"], min_dR: float
+) -> ak.Array:
+    jet_phi = events["Jet.Phi"].array()
+    jet_eta = events["Jet.Eta"].array()
+    lepton_phi = events[f"{lepton}.Phi"].array()
+    lepton_eta = events[f"{lepton}.Eta"].array()
+
+    lepton_dR_mask = []
+    for event_idx in tqdm(range(len(lepton_phi))):
+        jet_phi_idx = np.array(jet_phi[event_idx])
+        jet_eta_idx = np.array(jet_eta[event_idx])
+        lepton_dR_event_mask = np.ones_like(jet_phi_idx, dtype=int)
+        for lep_idx in range(len(lepton_phi[event_idx])):
+            dPhi = kinematics.normalize_dPhi(
+                jet_phi_idx - lepton_phi[event_idx][lep_idx]
+            )
+            dEta = jet_eta_idx - lepton_eta[event_idx][lep_idx]
+            dR = np.sqrt(dPhi**2 + dEta**2)
+            lepton_dR_event_mask *= dR > min_dR
+        lepton_dR_mask.append(lepton_dR_event_mask.astype(bool))
+    lepton_dR_mask = ak.from_iter(lepton_dR_mask)
+    return lepton_dR_mask
+
+
+def select_electron(events) -> ak.Array:
     """Create boolean mask to apply electron selection criteria from ATLAS
 
     :param events: Delphes event TTree containing
     :type events: TTree
     :return: boolean mask to select electrons in events
-    :rtype: JaggedArray
+    :rtype: Array
     """
+
+    pt_mask = get_electron_pt_mask(events=events)
+    eta_mask = get_electron_eta_mask(events=events)
+    jet_dR_mask = get_electron_to_jet_mask(events=events)
+    mask = pt_mask * eta_mask * jet_dR_mask
+    return mask
+
+
+def get_electron_pt_mask(events) -> ak.Array:
+    electron_pt = events["Electron.PT"].array()
+    pt_mask = electron_pt > 25
+    return pt_mask
+
+
+def get_electron_eta_mask(events) -> ak.Array:
+    electron_eta = events["Electron.Eta"].array()
+    eta_mask1 = (np.abs(electron_eta) < 2.5) * (np.abs(electron_eta) > 1.52)
+    eta_mask2 = np.abs(electron_eta) < 1.37
+    eta_mask = eta_mask1 + eta_mask2
+    return eta_mask
+
+
+def get_electron_to_jet_mask(events) -> ak.Array:
     jet_phi = events["Jet.Phi"].array()
     jet_eta = events["Jet.Eta"].array()
     jet_mass = events["Jet.Mass"].array()
     jet_pt = events["Jet.PT"].array()
-    electron_pt = events["Electron.PT"].array()
     electron_phi = events["Electron.Phi"].array()
     electron_eta = events["Electron.Eta"].array()
 
-    pt_mask = electron_pt > 25
-    eta_mask1 = (np.abs(electron_eta) < 2.5) * (np.abs(electron_eta) > 1.52)
-    eta_mask2 = np.abs(electron_eta) < 1.37
-    eta_mask = eta_mask1 + eta_mask2
     jet_dR_mask = []
-    for event_idx in range(len(events)):
-        electron_phi_idx = electron_phi[event_idx]
-        electron_eta_idx = electron_eta[event_idx]
+    for event_idx in tqdm(range(len(electron_phi))):
+        electron_phi_idx = np.array(electron_phi[event_idx])
+        electron_eta_idx = np.array(electron_eta[event_idx])
         jet_dR_event_mask = np.ones_like(electron_phi_idx, dtype=int)
         for jet_idx in range(len(jet_phi[event_idx])):
             jet_eta_idx = jet_eta[event_idx][jet_idx]
             jet_mass_idx = jet_mass[event_idx][jet_idx]
             jet_pt_idx = jet_pt[event_idx][jet_idx]
-            jet_rapidity = jet_eta_idx - (np.tanh(jet_eta_idx)/2)*(jet_mass_idx/jet_pt_idx)**2
-            dPhi = kinematics.normalize_dPhi(electron_phi_idx - jet_phi[event_idx][jet_idx])
+            jet_rapidity = (
+                jet_eta_idx
+                - (np.tanh(jet_eta_idx) / 2) * (jet_mass_idx / jet_pt_idx) ** 2
+            )
+            dPhi = kinematics.normalize_dPhi(
+                electron_phi_idx - jet_phi[event_idx][jet_idx]
+            )
             dEta = electron_eta_idx - jet_rapidity
             dR = np.sqrt(dPhi**2 + dEta**2)
-            jet_dR_event_mask *= (dR > 0.4)
+            jet_dR_event_mask *= dR > 0.4
         jet_dR_mask.append(jet_dR_event_mask.astype(bool))
-    jet_dR_mask = JaggedArray.fromiter(jet_dR_mask)
-    mask = pt_mask * eta_mask * jet_dR_mask
-    return mask
+    jet_dR_mask = ak.from_iter(jet_dR_mask)
+    return jet_dR_mask
 
 
-def select_muon(events) -> JaggedArray:
+def select_muon(events) -> ak.Array:
     """Create boolean mask to apply muon selection criteria from ATLAS
 
     :param events: Delphes event TTree containing
     :type events: TTree
     :return: boolean mask to select muon in events
-    :rtype: JaggedArray
+    :rtype: Array
     """
+    pt_mask = get_muon_pt_mask(events=events)
+    eta_mask = get_muon_eta_mask(events=events)
+    jet_dR_mask = get_muon_to_jet_mask(events=events)
+    mask = pt_mask * eta_mask * jet_dR_mask
+    return mask
+
+
+def get_muon_pt_mask(events) -> ak.Array:
+    muon_pt = events["Muon.PT"].array()
+    pt_mask = muon_pt > 25
+    return pt_mask
+
+
+def get_muon_eta_mask(events) -> ak.Array:
+    muon_eta = events["Muon.Eta"].array()
+    eta_mask = np.abs(muon_eta) < 2.5
+    return eta_mask
+
+
+def get_muon_to_jet_mask(events) -> ak.Array:
     jet_phi = events["Jet.Phi"].array()
     jet_eta = events["Jet.Eta"].array()
-    muon_pt = events["Muon.PT"].array()
     muon_phi = events["Muon.Phi"].array()
     muon_eta = events["Muon.Eta"].array()
 
-    pt_mask = muon_pt > 25
-    eta_mask = np.abs(muon_eta) < 2.5
     jet_dR_mask = []
-    for event_idx in range(len(events)):
-        muon_phi_idx = muon_phi[event_idx]
-        muon_eta_idx = muon_eta[event_idx]
+    for event_idx in tqdm(range(len(muon_phi))):
+        muon_phi_idx = np.array(muon_phi[event_idx])
+        muon_eta_idx = np.array(muon_eta[event_idx])
         jet_dR_event_mask = np.ones_like(muon_phi_idx, dtype=int)
         for jet_idx in range(len(jet_phi[event_idx])):
             dPhi = kinematics.normalize_dPhi(muon_phi_idx - jet_phi[event_idx][jet_idx])
             dEta = muon_eta_idx - jet_eta[event_idx][jet_idx]
             dR = np.sqrt(dPhi**2 + dEta**2)
-            jet_dR_event_mask *= (dR > 0.4)
+            jet_dR_event_mask *= dR > 0.4
         jet_dR_mask.append(jet_dR_event_mask.astype(bool))
-    jet_dR_mask = JaggedArray.fromiter(jet_dR_mask)
-    mask = pt_mask * eta_mask * jet_dR_mask
-    return mask
+    jet_dR_mask = ak.from_iter(jet_dR_mask)
+    return jet_dR_mask
