761007a1c643157fa287de838fc17c94402d67a1
cmpatino <carlosmiguelpatino@gmail.com>
2022-08-24 18:20:31 -0500

    Fix default path configuration

---
+++ src/hep-docker/sm-container/reco_code/reco_config.py
@@ -0,0 +1,9 @@
+import os
+
+# Reconstruction Config
+random_seed = 0
+root_file_path = (
+    "/root/reco_code/PROC_sm_0/"
+    "Events/run_01_decayed_1/tag_1_delphes_events.root"
+)
+recos_output_dir = "/root/reco_code/reconstructed_events"
