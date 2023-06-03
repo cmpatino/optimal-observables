f713c0fdc735c0b3c01ca4d5a3629342ba0d4fc0
cmpatino <carlosmiguelpatino@gmail.com>
2023-06-02 23:58:18 -0500

    Change condition for including cosine terms
    We previously set false for including additional cosine products as feature. We reverse the condition to include them when setting the parameter to True. That way it is consitent with include_mtt parameters.

--- src/optimal_observables/optimization/data.py
+++ src/optimal_observables/optimization/data.py
@@ -80,7 +80,7 @@ class ClassifierDataLoader:
         self,
         pos_reconstruction_paths: List[str],
         neg_reconstruction_paths: List[str],
-        only_cosine_terms: bool = True,
+        include_cosine_prods: bool = True,
         include_mtt: bool = True,
     ):
         pos_recos = ReconstructionLoader(
@@ -97,7 +97,7 @@ class ClassifierDataLoader:
             "cos_n1",
             "cos_n2",
         ]
-        if not only_cosine_terms:
+        if include_cosine_prods:
             self.feature_names.extend(
                 [
                     "cos_k1 * cos_k2",
@@ -111,21 +111,23 @@ class ClassifierDataLoader:
                     "(cos_n1 * cos_k2) - (cos_k1 * cos_n2)",
                 ]
             )
+
         pos_matrix = observables.get_angles_matrix(
             p_l_t=pos_recos["p_l_t"],
             p_l_tbar=pos_recos["p_l_tbar"],
             p_top=pos_recos["p_top"],
             p_tbar=pos_recos["p_tbar"],
-            only_cosine_terms=only_cosine_terms,
+            include_cosine_prods=include_cosine_prods,
         )
         neg_matrix = observables.get_angles_matrix(
             p_l_t=neg_recos["p_l_t"],
             p_l_tbar=neg_recos["p_l_tbar"],
             p_top=neg_recos["p_top"],
             p_tbar=neg_recos["p_tbar"],
-            only_cosine_terms=only_cosine_terms,
+            include_cosine_prods=include_cosine_prods,
         )
         self.X = np.concatenate([pos_matrix, neg_matrix], axis=0)
+
         if include_mtt:
             pos_mtt = observables.get_mtt(
                 p_top=pos_recos["p_top"],
