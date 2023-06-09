5e32b3870c9e3d09969a2358981d714e862e20a9
cmpatino <carlosmiguelpatino@gmail.com>
2023-06-09 18:30:00 -0500

    Create script to generate model feature
    The script generates features available for modeling and pushes the dataset to a Hugging Face dataset.

---
+++ src/generate_features.py
@@ -0,0 +1,51 @@
+import os
+
+import pandas as pd
+
+from datasets import load_dataset
+
+from optimal_observables.optimization import data
+
+
+if __name__ == "__main__":
+    base_path = "../data/output_200k_spin"
+    pos_process = "_SPIN_"
+    neg_process = "_NOSPIN_"
+
+    pos_reco_paths = [
+        os.path.join(base_path, path)
+        for path in os.listdir(base_path)
+        if pos_process in path
+    ]
+    neg_reco_paths = [
+        os.path.join(base_path, path)
+        for path in os.listdir(base_path)
+        if neg_process in path
+    ]
+
+    data_loader = data.ClassifierDataLoader(
+        pos_reconstruction_paths=pos_reco_paths,
+        neg_reconstruction_paths=neg_reco_paths,
+        include_cosine_prods=True,
+        include_mtt=True,
+        include_pt=True,
+        include_dPhi=True,
+        include_top_vecs=True,
+        include_lepton_vecs=True,
+        include_neutrino_vecs=True,
+        include_bjet_vecs=True,
+        include_reco_weights=True,
+    )
+    X, y = data_loader.load()
+
+    features_df = pd.DataFrame(X, columns=data_loader.feature_names)
+    features_df["target"] = y
+    features_df.to_csv("../artifacts/all_features.csv", index=False)
+
+    X_pos = X[y.reshape(-1) == 1]
+    X_neg = X[y.reshape(-1) == 0]
+    labels = ["spin-ON", "spin-OFF"]
+
+    # Create HuggingFace dataset from CSV
+    dataset = load_dataset("csv", data_files="../artifacts/all_features.csv")
+    dataset.push_to_hub("cmpatino/optimal-observables")
